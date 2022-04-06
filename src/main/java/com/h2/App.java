8d1574e3cb89ccebf5aa304c2f0d5cca0c855980
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-06 12:58:19 -0400

    steps 1-4

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -12,6 +12,13 @@ public class App
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number * 2;
+    }
+    private static int add(int[] numbers){
+        var sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        return sum;
     }
 }

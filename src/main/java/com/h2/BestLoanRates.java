2b70a60047c9c97faaf2c1ba79b9320021a088fa
Seth A Burleson <Sbrevolution5@aim.com>
2022-04-06 13:05:21 -0400

    Steps 5-8

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,5 +1,20 @@
 package com.h2;
 
-public class BestLoanRates {
+import java.util.Map;
+import java.util.Scanner;
 
+public class BestLoanRates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+    }
+
+    public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
+
+    public static float getRates(int loanTermInYears) {
+        return 0.0f;
+    }
 }
+
